0b8387edea0f3224e00ec435b6e6a944a72b098e
RandomlyCoded <marc+github@taschenorakel.de>
2026-03-08 12:10:54 +0100

    debug format
    at least in theorey

--- src/Remote/src/drone.cpp
+++ src/Remote/src/drone.cpp
@@ -47,6 +47,15 @@ QByteArray Drone::preparePayload()
     QDataStream s{&payload, QIODevice::WriteOnly};
     s.setByteOrder(QDataStream::LittleEndian);
 
+    s << quint8(128);
+
+    // writing the list directly would add separators etc.
+    for (const auto t: throttles())
+        s << quint8(clamp_u16(t) >> 8);
+
+
+    return payload;
+
     s << quint8(Backend::instance()->debugMode() << 7); // d000 0000
     s << quint8(Backend::instance()->debugMode() << 7); // d000 0000
     s << quint8(Backend::instance()->debugMode() << 7); // d000 0000
