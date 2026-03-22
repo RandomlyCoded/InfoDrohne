fe1e2980426a4cd825a46421f6f2312013bf6cdb
RandomlyCoded <marc+github@taschenorakel.de>
2026-03-22 11:52:46 +0100

    Dronen benennen
    einfachere Unterscheidung, wenn sie ausgegeben werden (z.B. über einen QML Text)

--- src/Remote/src/main.cpp
+++ src/Remote/src/main.cpp
@@ -22,6 +22,10 @@ int main(int argc, char *argv[])
     auto btDrone = new randomly::BtDrone(&app);
     auto backend = new randomly::Backend(btleDrone, udpDrone, btDrone, &app);
 
+    btleDrone->setObjectName("btleDrone");
+    btDrone->setObjectName("btDrone");
+    udpDrone->setObjectName("udpDrone");
+
     qmlRegisterSingletonInstance<randomly::UdpDrone>("DroneControl", 1, 0, "UdpDrone", udpDrone);
     qmlRegisterSingletonInstance<randomly::BtLEDrone>("DroneControl", 1, 0, "BtLEDrone", btleDrone);
     qmlRegisterSingletonInstance<randomly::Backend>("DroneControl", 1, 0, "Backend", backend);
